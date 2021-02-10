3e6f6786b502ba89fe13105dff6f1fe02b45fac7
Barrons <jrgranados.jg@gmail.com>
2021-02-10 14:23:16 -0600

    Create and read aggregated

---
+++ Students_CRUD/Models/StudenCE.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Students_CRUD.Models
+{
+    public class StudenCE
+    {
+    }
+    public partial class Student {
+        [Display(Name ="Nombre Completo")]
+      public string fullName { get { return name + " " + last_name; } }
+    }
+}
